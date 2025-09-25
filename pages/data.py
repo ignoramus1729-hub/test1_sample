99e319ce0cd58c3f1c8f20bd3cbcf479ffcdeec0
ignoramus1729-hub <ignoramus1729@mokwon.ac.kr>
2025-09-25 07:42:05 +0000

    새로운 페이지 추가'

--- pages/data.py
+++ pages/data.py
@@ -1,6 +1,11 @@
 
 # 간단한 데이터 시각화 예시 (선 그래프)
 
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 15, 25, 30]
+
+
+import streamlit as st
 import matplotlib.pyplot as plt
 from matplotlib import font_manager, rc
 
@@ -9,12 +14,12 @@ font_path = "./fonts/NanumGothic-Regular.ttf"
 font_name = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family=font_name)
 
-x = [1, 2, 3, 4, 5]
-y = [10, 20, 15, 25, 30]
+st.title('간단한 선 그래프 예시')
+st.write('아래는 임의의 데이터를 시각화한 선 그래프입니다.')
 
-plt.plot(x, y, marker='o')
-plt.title('간단한 선 그래프 예시')
-plt.xlabel('X축')
-plt.ylabel('Y축')
-plt.grid(True)
-plt.show()
+fig, ax = plt.subplots()
+ax.plot(x, y, marker='o')
+ax.set_xlabel('X축')
+ax.set_ylabel('Y축')
+ax.grid(True)
+st.pyplot(fig)
